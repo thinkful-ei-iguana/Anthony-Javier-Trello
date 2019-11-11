f40114f056c979a42aab5c473df2b29dee0dcdf8
Shaunna Beck <ShaunnaBeck@Shaunnas-MacBook-Pro.local>
2019-11-11 12:30:30 -0500

    added card and list comp.

---
+++ src/Card.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import STORE from './store';
+function cards(title,content){
+    return (
+        <div className='Card'>
+            <h3></h3>
+            <p></p>
+        </div>
+    );
+}
+
+export default {
+    cards,
+}
---
+++ src/List.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import STORE from './store';
+
+function list(header,cards){
+ return (  
+    <section className='List'>
+        <header>
+            {}
+        </header>
+        <div className='List-cards'></div>
+
+    </section>
+);
+}
+
+export default {
+    list
+}
