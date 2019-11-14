708b600f3f80108c17cafb3042541ee15eec6467
AnthonyTB <Anthonylol@protonmail.com>
2019-11-14 08:48:15 -0800

    refactored App to use state

--- src/App.js
+++ src/App.js
@@ -1,23 +1,31 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
-import List from './List'
-import STORE from './store';
-const store = STORE.STORE;
+import React from "react";
+import "./App.css";
+import List from "./List";
+import STORE from "./store";
 
-function App() {
+class App extends React.Component {
+  state = STORE.STORE;
 
-  let lists = store.lists.map( list => {
-    let id = list.cardIds.map( id => store.allCards[ id ] );
-    return <List key={list.id} header={list.header} cards={id}/>
-  } );
-
-  return ( <div className="App">
-    <header className="App-header">
-      <h1>Trelloyes!</h1>
-    </header>
-    <div className="App-list">{lists}</div>
-  </div> );
+  render() {
+    return (
+      <div className="App">
+        <header className="App-header">
+          <h1>Trelloyes!</h1>
+        </header>
+        <div className="App-list">
+          {this.state.lists.map(list => {
+            return (
+              <List
+                key={list.id}
+                header={list.header}
+                cards={list.cardIds.map(id => this.state.allCards[id])}
+              />
+            );
+          })}
+        </div>
+      </div>
+    );
+  }
 }
 
 export default App;
--- src/List.js
+++ src/List.js
@@ -1,25 +1,22 @@
-import React from 'react';
-import Card from './Card';
-import './styles/list.min.css';
+import React from "react";
+import Card from "./Card";
+import "./styles/list.min.css";
 
-export default function List( props ) {
-
-  console.log( props )
-
-  let cardsList = props.cards.map( ( card ) => {
-    return <Card title={card.title} content={card.content} key={card.id}/>
-  } )
-
-  return ( <section className='List'>
-    <header className='List-header'>
-      {props.header}
-    </header>
-    <div className='List-cards'>
-      {cardsList}
-      <button className='List-add-button'>
-        + Add random card
-      </button>
-    </div>
-
-  </section> );
+function List(props) {
+  console.log(props);
+  return (
+    <section className="List">
+      <header className="List-header">{props.header}</header>
+      <div className="List-cards">
+        {props.cards.map(card => {
+          return (
+            <Card title={card.title} content={card.content} key={card.id} />
+          );
+        })}
+        <button className="List-add-button">+ Add random card</button>
+      </div>
+    </section>
+  );
 }
+
+export default List;
